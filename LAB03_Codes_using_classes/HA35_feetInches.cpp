85bc610b7462cb3b031ffbf92869e62aeb6e5f21
Ajay Khatri <amajaying@gmail.com>
2022-08-03 09:37:54 +0530

    codes

--- LAB03_Codes_using_classes/HA35_feetInches.cpp
+++ LAB03_Codes_using_classes/HA35_feetInches.cpp
@@ -2,11 +2,36 @@
 using namespace std;
 
 class Calculate{
+    public:
     int feet;
     int inches;
+    int result;
 
     void takeInput(){
-        
+        cout<<"Enter distance: "<<endl;
+        cout<<"In feet: ";
+        cin>>feet;
+        cout<<"In inches: ";
+        cin>>inches;
+    }
+
+    void calc(Calculate x, Calculate y){
+        feet = x.feet + y.feet;
+        inches = x.inches + y.inches;
+        if(inches>=12){
+            feet++;
+            inches-=12;
+        } 
+    }
+
+    void display(){
+        cout<<"The sum is: "<<feet<<"feet "<<inches<<"inch"<<endl;
     }
 };
-int main(){}
+int main(){
+    Calculate a,b,c;
+    a.takeInput();
+    b.takeInput();
+    c.calc(a,b);
+    c.display();
+}

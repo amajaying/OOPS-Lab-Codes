cbafac64a0da62024f1adffc14ad3d96a926edc3
Ajay Khatri <amajaying@gmail.com>
2022-10-25 21:34:14 +0530

    codes

---
+++ LAB08_DynamicBinding/Q1_shape.cpp
@@ -0,0 +1,86 @@
+// Create a class shape. Derive three classes from it; Circle, Square and Triangle. Find area of each shape and display it, using virtual function.
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class Shape{
+    public:
+    // virtual void getData() = 0;
+    virtual void area() = 0;
+    virtual void display() = 0;
+};
+
+class Circle: public Shape{
+    float ar, pi;
+    int r;
+    public:
+    void getData(int r, float pi){
+        r=r;
+        pi = pi;
+    }
+    void area(){
+        ar = pi * r * r;
+    }
+    void display(){
+        cout<<"Area of Circle: "<< ar<<endl;
+    }
+};
+
+class Square: public Shape{
+    int ar;
+    int l;
+    public:
+    void getData(int l){
+        l = l;
+    }
+    void area(){
+        ar = l*l;
+    }
+    void display(){
+        cout<<"Area of Square: "<< ar<<endl;
+    }
+};
+class Triangle: public Shape{
+    int ar;
+    int a,b,c;
+    public:
+    void getData(int a1, int b1, int c1){
+        a=a1;
+        b=b1;
+        c=c1;
+    }
+    void area(){
+        int s = (a+b+c)/2;
+        ar = sqrt(s*(s-a)*(s-b)*(s-c));
+    }
+    void display(){
+        cout<<"Area of Triangle: "<<ar<<endl;
+    }
+};
+
+int main(){
+    int pi = 3.14;
+    Shape *t;
+    Triangle tr;
+    t=&tr;
+    tr.getData(5,6,7);
+    t->area();
+    t->display();
+
+    Square sq;
+    t=&sq;
+    sq.getData(5);
+    t->area();
+    t->display();
+
+    Circle c;
+    t=&c;
+    c.getData(5, 3.14);
+    t->area();
+    t->display();
+
+
+
+    return 0;
+}
---
+++ LAB08_DynamicBinding/Q2_employee.cpp
@@ -0,0 +1,4 @@
+// Create a class which stores employee name,id and salary Derive two classes from ‘Employee’ class: ‘Regular’ and ‘Part-Time’. The ‘Regular’ class stores DA, HRA and basic salary. The ‘Part-Time’ class stores the number of hours and pay per hour. Calculate the salary of a regular employee and a par-time employee, using virtual function
+
+#include<iostream>
+using namespace std;

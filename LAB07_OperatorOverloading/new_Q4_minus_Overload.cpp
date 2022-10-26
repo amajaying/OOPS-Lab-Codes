5b8f63c4f60ba9129f2d2267592f92260cc2bf29
Ajay Khatri <amajaying@gmail.com>
2022-10-26 09:12:07 +0530

    codes

---
+++ LAB07_OperatorOverloading/new_Q4_minus_Overload.cpp
@@ -0,0 +1,34 @@
+// 1. WAP to overload unary minus(-) operator using friend function and member function.
+
+#include <iostream>
+using namespace std;
+
+class Number{
+    int num;
+public:
+    Number(int n){
+        num = n;
+    }
+    void display(){
+        cout << "Number = " << num << endl;
+    }
+    friend Number operator-(Number);
+};
+
+Number operator-(Number n){
+    n.num = -n.num;
+    return n;
+}
+
+int main(){
+    Number n1(10), n2(-20);
+    cout<<"Before Overloading";
+    n1.display();
+    n2.display();
+    n1 = -n1;
+    n2 = -n2;
+    cout<<"After Overloading";
+    n1.display();
+    n2.display();
+    return 0;
+}
